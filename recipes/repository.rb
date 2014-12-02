3c279a09a5c13acbae1c422c17d8fe910f870fb5
Tim Smith <tim@cozy.co>
2014-12-02 10:17:01 -0800

    Add support for additional RHEL derivs

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -71,8 +71,8 @@ when 'debian'
     end
   end
 
-when 'amazon', 'fedora', 'centos', 'redhat'
-  unless platform?('centos', 'redhat') && node['platform_version'].to_f >= 6.4
+when 'amazon', 'fedora', 'centos', 'redhat', 'scientific', 'oracle'
+  unless platform?('centos', 'redhat', 'scientific', 'oracle') && node['platform_version'].to_f >= 6.4
     yum_repository 'remi' do
       description 'Remi'
       url node['php-fpm']['yum_url']
